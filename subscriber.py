d3efbd9cbab18bf93939be8f4851da896885dabc
Felipe Honorato <99209704+LipeHonorato@users.noreply.github.com>
2025-05-18 21:58:13 -0300

    Update subscriber.py

--- subscriber.py
+++ subscriber.py
@@ -6,7 +6,19 @@ s = context.socket(zmq.SUB)          # create a subscriber socket
 p = "tcp://"+ HOST +":"+ PORT        # how and where to communicate
 s.connect(p)                         # connect to the server
 s.setsockopt_string(zmq.SUBSCRIBE, "TIME")  # subscribe to TIME messages
+s.setsockopt_string(zmq.SUBSCRIBE, "SUM")  # subscribe to SUM messages
+s.setsockopt_string(zmq.SUBSCRIBE, "DICE")  # subscribe to DICE messages
+
+
 
 for i in range(5):  # Five iterations
 	time = s.recv()   # receive a message
 	print (bytes.decode(time))
+
+	sum = s.recv() # receive a message
+	print(bytes.decode(sum))
+
+	dice = s.recv() # receive a message
+	print(bytes.decode(dice))
+
+	print("====================================")
