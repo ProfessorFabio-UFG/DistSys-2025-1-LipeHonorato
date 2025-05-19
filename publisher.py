12487097121c8ab452adadf8a389e31183774025
Felipe Honorato <99209704+LipeHonorato@users.noreply.github.com>
2025-05-18 21:57:55 -0300

    Update publisher.py

--- publisher.py
+++ publisher.py
@@ -1,5 +1,19 @@
-import zmq, time
+import zmq, time, random
 from constPS import * #-
+
+def sum():
+	value1 = random.randint(1,10)
+	value2 = random.randint(1,10)
+	soma = value1 + value2
+	return "Sum from " + str(value1) + " and " + str(value2) + " = " + str(soma)
+
+def loadedDice():
+	if random.randint(1,5) == 3:
+		a = random.randint(1,6)
+		return "The dice roll was: " + str(a)
+	else: 
+		return "The dice roll was: 4"
+
 
 context = zmq.Context()
 s = context.socket(zmq.PUB)        # create a publisher socket
@@ -9,3 +23,10 @@ while True:
 	time.sleep(5)                    # wait every 5 seconds
 	msg = str.encode("TIME " + time.asctime())
 	s.send(msg) # publish the current time
+
+	msg1 = str.encode("SUM " + sum())
+	s.send(msg1) # publish the msg1
+
+	msg2 = str.encode("DICE " + loadedDice())
+	s.send(msg2) # publish the msg2
+
